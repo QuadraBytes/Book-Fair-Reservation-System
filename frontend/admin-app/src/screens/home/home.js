75dec30fa50b7a3e39b4807d762ade677f0283df
MoraisMNS <nirashamorais@gmail.com>
2025-10-23 16:11:07 +0530

    First commit

--- frontend/admin-app/src/screens/home/home.js
+++ frontend/admin-app/src/screens/home/home.js
@@ -5,6 +5,7 @@ function Home() {
     return (
         <div className="flex h-screen bg-gray-50">
             <h2 className="text-lg font-semibold ml-4">Home Admin</h2>
+            <h1 className="text-lg font-semibold ml-4">This is the home page </h1>
         </div>
     )
 }
