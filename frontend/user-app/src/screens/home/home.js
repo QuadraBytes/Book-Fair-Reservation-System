f0e1ebe56b88ddeff66357aa86b098a8c81ffc2b
Nikini11 <karunarathnanikini@gmail.com>
2025-10-24 23:59:23 +0530

    commit 01

--- frontend/user-app/src/screens/home/home.js
+++ frontend/user-app/src/screens/home/home.js
@@ -4,7 +4,7 @@ import React from 'react'
 function Home() {
     return (
         <div className="flex h-screen bg-gray-50">
-            <h2 className="text-lg font-semibold ml-4">Home Admin</h2>
+            <h2 className="text-lg font-semibold ml-4">Home Public User</h2>
         </div>
     )
 }
