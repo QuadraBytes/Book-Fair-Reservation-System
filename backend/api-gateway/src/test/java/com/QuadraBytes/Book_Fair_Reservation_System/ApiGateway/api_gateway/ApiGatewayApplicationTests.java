a466742ee14c6d4bfef0e76001b1d3aac98a0f49
morawaliyadda <sithara.morawaliyadda2000@gmail.com>
2025-11-11 22:38:04 +0530

    initial set up for api-gateway

---
+++ backend/api-gateway/src/test/java/com/QuadraBytes/Book_Fair_Reservation_System/ApiGateway/api_gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.QuadraBytes.Book_Fair_Reservation_System.ApiGateway.api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
