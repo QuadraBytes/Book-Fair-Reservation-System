6feb95283af579f33f6d60f3c427dd8f7fe85d7b
AhamedMinhaj456 <139200872+AhamedMinhaj456@users.noreply.github.com>
2025-11-26 22:56:39 +0530

    notification service enabled

--- backend/Reservation/src/main/java/com/QuadraBytes/Book_Fair_Reservation_System/Reservation/service/QRVerificationService.java
+++ backend/Reservation/src/main/java/com/QuadraBytes/Book_Fair_Reservation_System/Reservation/service/QRVerificationService.java
@@ -26,8 +26,8 @@ public class QRVerificationService {
     @Autowired
     private QRVerificationRepo qrVerificationRepo;
 
-//    @Value("${app.base-url=http://173.249.12.92:9080/reservation-service/api/}")
-    @Value("${app.base-url=http://localhost:9080/reservation-service/api}")
+    @Value("${app.base-url=http://173.249.12.92:9080/reservation-service/api/}")
+//    @Value("${app.base-url=http://localhost:9080/reservation-service/api}")
     private String appBaseUrl;
 
     private static final String OUTPUT_DIR = "uploads/qr/";
